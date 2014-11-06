d216293fb02827c532efede477fb2692de7e8d66
ore13048 <henryale_37@hotmail.com>
2014-11-05 20:02:53 -0600

    Create Nodo.java

---
+++ Nodo.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Nodo<T extends Comparable> {
+    //ATRIBUTOS
+    private T nombre;
+    private String[] vecinos;
+    
+    //METODOS
+    public Nodo(int dimension, T nombre){
+        this.nombre = nombre;
+        this.vecinos = new String[dimension];
+        for(int i = 0; i<dimension; i++){
+            vecinos[i] = "9999";
+        }
+    }
+    
+    public void agregarVecino(int valor, int columna){
+        vecinos[columna] = valor+"";
+    }
+    
+    public void eliminarVecino(int columna){
+        vecinos[columna] = "0";
+    }
+    
+    public T getNombre(){
+        return nombre;
+    }
+    
+    public void setNombre(T nombre){
+        this.nombre = nombre;
+    }
+    
+    public String[] getVecinos(){
+        return vecinos;
+    }
+}
